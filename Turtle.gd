0f5583e41dd13aca349d12c42e05f1356d77c17a
jammin-ben <benjamin.reber19@houghton.edu>
2020-03-10 16:28:30 -0400

    initial commit

---
+++ Turtle.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+const TARGET_FPS = 60
+const ACCELERATION = 8
+const MAX_SPEED = 64
+const FRICTION = 10
+const AIR_RESISTANCE = 1
+const GRAVITY = 4
+const JUMP_FORCE = 140
+
+var motion = Vector2.ZERO
+
+onready var sprite = $Turtle_Spr
+onready var animationPlayer = $AnimationPlayer
+
+func _physics_process(delta):
+	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if x_input != 0:
+		animationPlayer.play("Move")
+		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		sprite.flip_h = x_input < 0
+	else:
+		animationPlayer.play("Stand")
+	
+	motion.y += GRAVITY * delta * TARGET_FPS
+	
+	motion = move_and_slide(motion, Vector2.UP)
+	
+	if is_on_floor():
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, FRICTION * delta)
+			
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
+	else:
+		animationPlayer.play("Jump")
+		
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
+		
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
+	
