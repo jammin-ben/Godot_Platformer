bc3f97cea018f91ddd3935c0c6d7a04e7c7fb8d5
jammin-ben <benjamin.reber19@houghton.edu>
2020-04-10 18:28:17 -0400

    Cleaned up turt collisions. Halted the sky in its tracks (for now)

--- Turtle.gd
+++ Turtle.gd
@@ -7,25 +7,30 @@ const FRICTION = 10
 const AIR_RESISTANCE = 1
 const GRAVITY = 6
 const JUMP_FORCE = 220
+const SKY_SPEED = 35
 
 var motion = Vector2.ZERO
 
 onready var sprite = $Turtle_Spr
 onready var animationPlayer = $AnimationPlayer
 onready var clouds = $ParallaxBackground/Cloud_Layer
-onready var sunrise = $ParallaxBackground/Sunrise/AnimationPlayer
 onready var sky = $ParallaxBackground/Sky_Layer
+onready var sunset = $ParallaxBackground/Sky_Layer/Sunrise
+onready var rray = $RayCastRight
+onready var lray = $RayCastLeft
 
-func _ready():
-	sky.motion_offset.y = -550
-	
 func _process(delta):
 	clouds.motion_offset.x += 2*delta
-	sky.motion_offset.y -= 8*delta
-	if(sky.motion_offset.y < -675 and not sunrise.is_playing()):
-		sunrise.play("Sunrise_Anim")
-		sky.motion_offset.y=0
-
+	#sky.motion_offset.y -= SKY_SPEED*delta
+	#if(sky.motion_offset.y < -600):
+	#	pass
+#		sunset.offset.y+=SKY_SPEED*delta
+	#if(sky.motion_offset.y < -675):
+	#	sky.motion_offset.y=0
+#		sunset.offset.y=0
+func is_on_ground():
+	return lray.is_colliding() or rray.is_colliding()
+	
 func _physics_process(delta):
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
@@ -38,8 +43,7 @@ func _physics_process(delta):
 		if(animationPlayer.current_animation=="Move"):
 			animationPlayer.play("Stand")
 	
-	if is_on_floor():
-		#print("floor")
+	if is_on_ground():
 		if x_input == 0:
 			if(Input.is_action_just_pressed("ui_down")):
 				motion.x=0
