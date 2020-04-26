bd463e2b6289e7041e0f6b088fdb8147ddb64417
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2020-04-26 01:31:38 -0400

    Got the switching mostly working

--- CompositTurtle.gd
+++ CompositTurtle.gd
@@ -5,21 +5,70 @@ extends Node2D
 # var a: int = 2
 # var b: String = "text"
 
-onready var default_turt = $TurtleDefault
-onready var ball_turt = $TurtleBall
+export var ball_mode = false setget set_ball_mode
+
+onready var turt_default = $TurtleDefault
+onready var turt_ball = $TurtleBall
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	default_turt.connect("Hidden", self, "on_turtle_hidden")
-	default_turt.connect("Emerged", self, "on_turtle_emerged")
+	turt_default.connect("Hidden", self, "on_turtle_hidden")
+	turt_default.connect("Emerged", self, "on_turtle_emerged")
 	pass # Replace with function body.
 
 func on_turtle_hidden():
-	print("The turtle is now hidden")
+	set_ball_mode(true)
 
 func on_turtle_emerged():
-	print("The turtle has now emerged")
+	set_ball_mode(false)
 
+
+func set_ball_mode(value: bool):
+	ball_mode = value
+	if (ball_mode):
+		_set_ball_mode()
+	else:
+		_set_default_mode()
+
+
+func _disable_turt_ball():
+	#turt_ball.visible = false
+	$TurtleBall/Camera2D.current = false
+	
+	$TurtleBall/CollisionPolygon2D.disabled = true
+	turt_ball.mode =RigidBody2D.MODE_STATIC
+	turt_ball.sleeping = true
+
+func _enable_turt_ball():
+	#turt_ball.visible = true
+	$TurtleBall/Camera2D.current = true
+	
+	$TurtleBall/CollisionPolygon2D.disabled = false
+	turt_ball.mode = RigidBody2D.MODE_RIGID
+	turt_ball.sleeping = false
+
+func _disable_turt_default():
+	#turt_default.visible = false
+	$TurtleDefault/Camera2D.current = false
+	$TurtleDefault/CollisionShape2D.disabled = true
+	turt_default.set_physics_process(false)
+	
+func _enable_turt_default():
+	#turt_default.visible = true
+	$TurtleDefault/Camera2D.current = true
+	$TurtleDefault/CollisionShape2D.disabled = false
+	turt_default.set_physics_process(true)
+
+func _set_default_mode():
+	_disable_turt_ball()
+	_enable_turt_default()
+
+func _set_ball_mode():
+	_disable_turt_default()
+	_enable_turt_ball()
+	turt_ball.position = turt_default.position
+	turt_ball.position.y -= 2
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
