a503ec8b7b53c2cd79e68c51780e77a1c5cf318e
jammin-ben <benjamin.reber19@houghton.edu>
2020-10-19 22:50:03 -0400

    Added menu screen visuals

--- scenery/ParallaxBackground.gd
+++ scenery/ParallaxBackground.gd
@@ -3,7 +3,11 @@ extends ParallaxBackground
 onready var clouds = $Cloud_Layer
 onready var sky = $Sky_Layer
 onready var sunset = $Sky_Layer/Sunrise
+onready var forest = $Forest_Layer
+onready var mountains = $Mountain_Layer2 
+onready var mountains_far = $Mountain_Layer_Far 
 
+export var menu_version = false
 
 # Declare member variables here. Examples:
 # var a: int = 2
@@ -18,3 +22,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	clouds.motion_offset.x += 2*delta
+	if(menu_version):
+		forest.motion_offset.x -= 2*delta
+		mountains.motion_offset.x -= delta / 2
+		mountains_far.motion_offset.x -= delta / 3
+		
