8b76fead62139e564873d3ae305386e0ed48879a
jammin-ben <benjamin.reber19@houghton.edu>
2020-11-01 22:29:09 -0500

    Added really nice smooth camera motion. Except it doesn't quite work

---
+++ Player_Camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var turt = get_parent().get_node("CompositTurtle/TurtleDefault")
+onready var turt_parent = get_parent().get_node("CompositTurtle")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var target= turt.position+turt_parent.position
+	self.position += .005*(target - position)
