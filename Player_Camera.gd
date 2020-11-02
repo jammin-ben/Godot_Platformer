802914d55b524460520ff4e95943806a406ad321
jammin-ben <benjamin.reber19@houghton.edu>
2020-11-02 14:11:18 -0500

    Turned off vsync and set camera to follow more quickly when falling.

--- Player_Camera.gd
+++ Player_Camera.gd
@@ -9,10 +9,15 @@ onready var turt = get_parent().get_node("CompositTurtle/TurtleDefault")
 onready var turt_parent = get_parent().get_node("CompositTurtle")
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var weights = Vector2(2,2)
 	var target= turt.position+turt_parent.position
-	self.position += 2* delta*(target - position)
+	#self.position += 2 * delta * (target - position)
+	if(target.y  - self.position.y> 50):
+		weights.y = 4
+	self.position += delta * weights * (target - position)
