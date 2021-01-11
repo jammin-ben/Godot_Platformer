0b0deaa77e047516ce2eda6836e314ed1e7597fd
jammin-ben <benjamin.reber19@houghton.edu>
2021-01-11 00:01:27 -0500

    Bun now only moves in full-hop increments. Rand is no longer called every frame in bun and beaver

--- scenery/Beaver.gd
+++ scenery/Beaver.gd
@@ -12,9 +12,14 @@ onready var lray = $lray
 
 onready var raycast = rray
 
+export var time_chunk = .6
+export var max_run_chunks = 4
+export var max_wait_chunks = 8
+
 func stand():
 	animationPlayer.play("Stand")
 	dir=0
+	$Timer.start(ceil(randf()*max_wait_chunks)*time_chunk)
 
 func run():
 	dir = floor(randf()*2) 
@@ -30,7 +35,7 @@ func run():
 		raycast = rray
 	else:
 		raycast = lray
-		
+	$Timer.start(ceil(randf()*max_run_chunks)*time_chunk)
 func _ready():
 	run()
 
@@ -39,11 +44,16 @@ func _physics_process(delta):
 		motion.x = speed * dir * delta * 60
 		sprite.flip_h = (dir < 0)
 		motion = move_and_slide(motion, Vector2.UP)
-		if(!raycast.is_colliding() or randf() < .02):
+		if(!raycast.is_colliding()): #or randf() < .02):
 			stand()
-	else:
+	#else:
 		#standing
-		if(randf()<.01):
-			run()
+	#	if(randf()<.01):
+	#		run()
 			
 	
+func _on_Timer_timeout():
+	if(dir == 0):
+		run()
+	else:
+		stand()
